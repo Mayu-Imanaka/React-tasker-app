1b0f247e1ecb7ce3faa41bbee3ef1e71a898b506
Mayu Imanaka <imamayu.1215@gmail.com>
2020-04-24 22:19:30 -0700

    add:complete tod list

---
+++ front-end/src/containers/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="logo" src="../sample_logo.jpg" alt="check" />
+        </div>
+    )
+}
+
+export default Nav;
